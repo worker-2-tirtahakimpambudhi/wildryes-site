9abe9935395abf6bd2a026d3bb2e51eaabf5d0db
worker-2-tirtahakimpambudhi <akucintaclaude@gmail.com>
2025-06-21 16:22:09 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_TvvGNuuyw', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_TvvGNuuyw', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '695rql9t5caoldda5qb0m9i1eb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
