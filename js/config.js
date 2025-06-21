fab96f45f6eebbde1c7f3d907e6151f58b7226c0
worker-2-tirtahakimpambudhi <akucintaclaude@gmail.com>
2025-06-21 16:04:11 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_TvvGNuuyw', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '695rql9t5caoldda5qb0m9i1eb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
